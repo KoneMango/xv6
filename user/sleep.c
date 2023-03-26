f76d2e4df27448ff2340c2e26f9d3287303848e5
KoneMango <98529557+KoneMango@users.noreply.github.com>
2023-03-26 22:45:28 +1100

    sleep

--- user/sleep.c
+++ user/sleep.c
@@ -1,1 +1,18 @@
-print("TryYesYes");
+#include "kernel/types.h"
+#include "kernel/stat.h"
+#include "user/user.h"
+
+//sleep.c
+int main(int argc, char *argv[])
+{
+    int n;
+    if (argc < 2)
+    {
+        fprintf(2, "usage: sleep n\n");
+        exit(1);
+    }
+    n = atoi(argv[1]);
+    sleep(n);
+    exit(0);
+}
+
