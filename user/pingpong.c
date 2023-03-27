4314d04d0e3336ca71bfe57ff9df73ad7554cfbb
KoneMango <98529557+KoneMango@users.noreply.github.com>
2023-03-27 20:38:59 +1100

    pingpong_ok

--- user/pingpong.c
+++ user/pingpong.c
@@ -2,15 +2,16 @@
 #include "kernel/stat.h"
 #include "user/user.h"
 #include <sys/types.h>
+#include "kernel/fcntl.h"
 
 int main(int argc, char* argv[])
 {
     pid_t pid;
     char buffer[20];
-
-    pid = fork();
     int fd1[2];
     pipe(fd1);
+    pid = fork();
+
     if (pid == 0) // child process(read)
     {
         close(fd1[1]);
@@ -23,9 +24,8 @@ int main(int argc, char* argv[])
         close(fd1[0]);
         write(fd1[1], "h", 1);
         close(fd1[1]);
+        wait(0);
         exit(0);
     }
-
     return 0;
-
 }
