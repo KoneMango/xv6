7256321c73e2c25b6269c70dd99755469b2160f8
KoneMango <98529557+KoneMango@users.noreply.github.com>
2023-03-28 19:33:30 +1100

    prime

--- user/primes.c
+++ user/primes.c
@@ -27,7 +27,7 @@ int main(int argc, char* argv[]){
         {
             printf("prime %d\n", arr[i]);
         }
-        pid = fork();
+        // pid = fork();
         exit(0);
     } 
     else  // parent process(write)
