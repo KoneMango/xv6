e10b1247332f1166e088eb89497fd2ba96fbed34
KoneMango <98529557+KoneMango@users.noreply.github.com>
2023-03-28 18:50:24 +1100

    prime

--- user/primes.c
+++ user/primes.c
@@ -20,19 +20,18 @@ int main(int argc, char* argv[]){
     pid=fork();
     if (pid == 0) // child process(read)
     {
-        //读取管道里的数组
-        read(fd[0][0], arr, 100);
-        close(fd[0][0]);
         close(fd[0][1]);
-        for (size_t i = 0; i < sizeof(arr) / sizeof(arr[0])-5; i++)
+        read(fd[0][0], arr, sizeof(arr));
+        close(fd[0][0]);
+        for (size_t i = 0; i < sizeof(arr) / sizeof(arr[0]); i++)
         {
             printf("prime %d\n", arr[i]);
         }
-        
         exit(0);
     } 
     else  // parent process(write)
     {
+        close(fd[0][0]);
         for (size_t i = 0; i < sizeof(arr) / sizeof(arr[0])-5; i++)
         {
             if (arr[i] % arr[0] == 0)
@@ -44,9 +43,8 @@ int main(int argc, char* argv[]){
                 }
             }
         }
-        write(fd[0][1], arr, 100);
+        write(fd[0][1], arr, sizeof(arr));
         close(fd[0][1]);
-        close(fd[0][0]);
         wait(0);
         exit(0);
     }
