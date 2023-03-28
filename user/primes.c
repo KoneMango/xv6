fe036b24c82530e9324fee38ddfd6ba2632a49b8
KoneMango <98529557+KoneMango@users.noreply.github.com>
2023-03-28 16:37:38 +1100

    prime

--- user/primes.c
+++ user/primes.c
@@ -5,5 +5,43 @@
 #include "kernel/fcntl.h"
 
 int main(int argc, char* argv[]){
-    
+    pid_t pid;
+    int fd[10][2];
+    pid = fork();
+    int arr[35];
+    // 初始化数组
+    for (int i = 2; i < 35; i++) 
+    {
+        arr[i] = i + 1;
+    }
+    pid=fork();
+    if (pid == 0) // child process(read)
+    {
+        read(fd[0][0], arr[10], sizeof(arr[10]));
+        
+        exit(0);
+    } 
+    else  // parent process(write)
+    {
+        for (size_t i = 0; i < sizeof(arr); i++)
+        {
+            if (arr[i] % arr[0] == 0)
+            {
+                //在数组中去除arr[i]
+                for (size_t j = i; j < sizeof(arr); j++)
+                {
+                    arr[j] = arr[j + 1];
+                }
+            }
+        }
+        write(fd[0][1], arr, 100);
+        printf(arr);
+
+        wait(0);
+        exit(0);
+    }
+
+
+
+
 }
