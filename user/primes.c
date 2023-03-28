3cf17d648c6ea7116872f295238ddf77a1fe31b6
KoneMango <98529557+KoneMango@users.noreply.github.com>
2023-03-28 18:38:46 +1100

    prime

--- user/primes.c
+++ user/primes.c
@@ -23,16 +23,17 @@ int main(int argc, char* argv[]){
         //读取管道里的数组
         read(fd[0][0], arr, 100);
         close(fd[0][0]);
+        close(fd[0][1]);
         exit(0);
     } 
     else  // parent process(write)
     {
-        for (size_t i = 0; i < sizeof(arr)-5; i++)
+        for (size_t i = 0; i < sizeof(arr) / sizeof(arr[0])-5; i++)
         {
             if (arr[i] % arr[0] == 0)
             {
                 //在数组中去除arr[i]
-                for (size_t j = i; j < sizeof(arr); j++)
+                for (size_t j = i; j < sizeof(arr) / sizeof(arr[0])-5; j++)
                 {
                     arr[j] = arr[j + 1];
                 }
@@ -40,7 +41,8 @@ int main(int argc, char* argv[]){
         }
         write(fd[0][1], arr, 100);
         close(fd[0][1]);
-        for (size_t i = 0; i < sizeof(arr)-5; i++)
+        close(fd[0][0]);
+        for (size_t i = 0; i < sizeof(arr) / sizeof(arr[0])-5; i++)
         {
             printf("prime %d\n", arr[i]);
         }
