93aec5c6b54569a8f4abc5769589c0711dc946e5
KoneMango <98529557+KoneMango@users.noreply.github.com>
2023-04-01 00:24:14 +1100

    prime4

--- user/primes.c
+++ user/primes.c
@@ -4,57 +4,61 @@
 #include <sys/types.h>
 #include "kernel/fcntl.h"
 
-int main(int argc, char* argv[]){
-    pid_t pid;
-    int fd[2];
-    pipe(fd);
-    pid = fork();
-    int index = 0; 
+void sieve(int *arr, int index, int size) {
+    int prime = arr[index];
+    int i, j = 0;
+    for (i = 0; i < size; i++) {
+        if (arr[i] % prime != 0 || arr[i] == prime) {
+            arr[j++] = arr[i];
+        }
+    }
+    for (; j < size; j++) {
+        arr[j] = 0;
+    }
+}
 
-
+int main(int argc, char* argv[]) {
     int arr[34];
     // 初始化数组
-    for (int i = 2; i < 35; i++) 
-    {
-        arr[i-2] = i;
+    for (int i = 2; i < 36; i++) {
+        arr[i - 2] = i;
     }
 
-    pid=fork();
-    if (pid == 0) // child process(read)
-    {
-        close(fd[1]);
-        read(fd[0], arr, sizeof(arr));
-        for (size_t i = 1 ; i < sizeof(arr) / sizeof(arr[0]); i++)
-        {
-            if(arr[i]%arr[index]==0)
-            {
-                arr[i]=arr[i+1];
+    int fd[2];
+    int index = 0;
+    pid_t pid;
+
+    while (arr[index] != 0 && arr[index] * arr[index] <= arr[sizeof(arr) / sizeof(arr[0]) - 1]) {
+        pipe(fd);
+        pid = fork();
+
+        if (pid == 0) { // child process(read)
+            close(fd[1]);
+            read(fd[0], arr, sizeof(arr));
+            sieve(arr, index, sizeof(arr) / sizeof(arr[0]));
+            index++;
+            close(fd[0]);
+            pipe(fd);
+            pid = fork();
+            if (pid != 0) {
+                wait(0);
+                break;
+            }
+        } else { // parent process(write)
+            close(fd[0]);
+            write(fd[1], arr, sizeof(arr));
+            close(fd[1]);
+            wait(0);
+        }
+    }
+
+    if (pid != 0) {
+        // 输出质数
+        for (int i = 0; i < sizeof(arr) / sizeof(arr[0]); i++) {
+            if (arr[i] != 0) {
+                printf("prime %d\n", arr[i]);
             }
         }
-        printf("%d", arr[index]);
-        index ++;
-        close(fd[0]);
-        // for (size_t i = 0; i < sizeof(arr) / sizeof(arr[0]); i++)
-        // {
-        //     printf("prime %d\n", arr[i]);
-        // }
-        exit(0);
-    } 
-    else  // parent process(write)
-    {
-        close(fd[0]);
-        // printf("%d",arr[0]);
-        // for (size_t i = 0; i < sizeof(arr) / sizeof(arr[0])-5; i++)
-        // {
-        //     if (arr[i]%arr[0] ==0)
-        //     {
-        //         arr[i] = arr[i+1];
-        //     }
-        // }
-        write(fd[1], arr, sizeof(arr));
-        close(fd[1]);
-        pid = fork();
-        wait(0);
-        exit(0);
     }
+    exit(0);
 }
