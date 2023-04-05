41ebb3a45c62c5da1019acd959eb7c989a97fdc1
KoneMango <98529557+KoneMango@users.noreply.github.com>
2023-04-05 15:38:46 +1000

    prime4

--- user/xargs.c
+++ user/xargs.c
@@ -1,64 +1,54 @@
 #include "kernel/types.h"
 #include "user/user.h"
-#include "kernel/param.h"
+#include "kernel/fcntl.h"
 
-int main(int argc, char *argv[])
-{
-    int p[2];
-    int i, j, n;
-    char buf[4];
-    int prime;
-    int count = 0;
-    int arr[MAXARG];
+#define MAXARG 32    //最大参数个数
+#define MAXARGLEN 64 //最大参数长度
 
-    if (argc < 2) {
-        fprintf(2, "Usage: xargs command\n");
+int main(int argc, char *argv[]) {
+    char *args[MAXARG]; //参数列表
+    char buf[MAXARGLEN]; //缓冲区
+    int nargs = 0; //参数个数
+    int i, n;
+
+    if (argc < 2) { //参数错误
+        fprintf(2, "usage: xargs cmd [args ...]\n");
         exit(1);
     }
 
-    // 从标准输入读取数据
-    while (read(0, buf, 4) > 0) {
-        arr[count++] = atoi(buf);
-    }
+    //读取标准输入，并将每个字符串作为参数传递给指定命令
+    while ((n = read(0, buf, MAXARGLEN)) > 0) { //从标准输入读取数据
+        for (i = 0; i < n; i++) { //遍历缓冲区中的每个字符
+            if (buf[i] == '\n') { //如果遇到换行符，则将参数列表传递给指定命令
+                args[nargs] = 0; //最后一个参数必须为NULL
 
-    // 创建管道
-    pipe(p);
+                //子进程操作
+                if (fork() == 0) { //创建子进程，执行指定命令，并传递参数
+                    exec(argv[1], args); //执行指定命令，并传递参数
+                    fprintf(2, "xargs: exec %s failed\n", argv[1]); //指定命令执行失败
+                    exit(1);
+                } 
 
-    // 创建子进程
-    if (fork() == 0) {
-        // 子进程关闭读端
-        close(p[0]);
-
-        // 将当前数组中的所有数写入管道
-        for (i = 0; i < count; i++) {
-            write(p[1], (char *)(arr + i), 4);
-        }
-
-        // 子进程关闭写端，并退出
-        close(p[1]);
-        exit(0);
-    } else {
-        // 父进程关闭写端
-        close(p[1]);
-
-        // 重置count为0，用于统计剩余未被筛选的数
-        count = 0;
-
-        // 从管道中读取数据
-        while (read(p[0], buf, 4) > 0) {
-            n = atoi(buf);
-            if (n % arr[0] != 0) {
-                arr[count++] = n;
+                //父进程操作
+                else {
+                    wait(0); //等待子进程结束
+                }
+                nargs = 0; //清空参数列表，以便下一次使用
+            } 
+        
+            else if (isspace(buf[i])) { //如果遇到空格或制表符，则将它们转换为NULL字符，作为参数分隔符
+                buf[i] = 0;
+            } else {
+                args[nargs++] = &buf[i]; //将参数添加到参数列表中
+                while (!isspace(buf[i]) && i < n) i++; //跳过单词，找到下一个分隔符
+                i--; //让下次循环能继续读取这个单词的字符
+            }
+            if (nargs >= MAXARG) { //参数个数超过最大限制
+                fprintf(2, "xargs: too many arguments\n");
+                exit(1);
             }
         }
-
-        // 关闭管道
-        close(p[0]);
-
-        // 递归调用printPrime函数
-        printPrime(arr, count);
     }
 
     exit(0);
-
 }
