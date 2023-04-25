1cf6dec2c545dc8222ad9117a909aeae662e979a
KoneMango <98529557+KoneMango@users.noreply.github.com>
2023-04-25 17:32:51 +1000

    pgtbl_pg

--- kernel/defs.h
+++ kernel/defs.h
@@ -109,7 +109,7 @@ int             either_copyout(int user_dst, uint64 dst, void *src, uint64 len);
 int             either_copyin(void *dst, int user_src, uint64 src, uint64 len);
 void            procdump(void);
 void            proc_freekpt(pagetable_t pagetable);     
-static void     freeproc(struct proc *p);
+// static void     freeproc(struct proc *p);
 // swtch.S
 void            swtch(struct context*, struct context*);
 
